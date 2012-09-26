d6225eaf6f8a585273618929a197cae4128b130a
heatherjc07 <heather_j_milligan@hotmail.com>
2012-09-26 23:22:33 +0100

    Adding day 3 Prolog

---
+++ Prolog/Day3/queens.pl
@@ -0,0 +1,35 @@
+?- [library(clpfd)].
+
+valid_queen((Row, Col)) :- member(Col, [1,2,3,4,5,6,7,8]).
+
+valid_board([]).
+valid_board([Head|Tail]) :- valid_queen(Head), valid_board(Tail).
+
+cols([], []).
+cols([(_, Col)|QueensTail], [Col|ColsTail]) :-
+  cols(QueensTail, ColsTail).
+
+diags1([], []).
+diags1([(Row, Col)|QueensTail], [Diagonal|DiagonalsTail]) :-
+  Diagonal is Col - Row,
+  diags1(QueensTail, DiagonalsTail).
+
+diags2([], []).
+diags2([(Row, Col)|QueensTail], [Diagonal|DiagonalsTail]) :-
+  Diagonal is Col + Row,
+  diags2(QueensTail, DiagonalsTail).
+
+eight_queens(Board) :-
+  Board = [(1, _), (2, _), (3, _), (4, _), (5, _), (6, _), (7, _), (8, _)],
+  valid_board(Board),
+
+  cols(Board, Cols),
+  diags1(Board, Diags1),
+  diags2(Board, Diags2),
+
+  all_different(Cols), % in the book, this is 'fd_all_different'
+  all_different(Diags1), % in the book, this is 'fd_all_different'
+  all_different(Diags2). % in the book, this is 'fd_all_different'
+
+% can test with:
+% ?- eight_queens(Board).
---
+++ Prolog/Day3/sudoku4x4.pl
@@ -0,0 +1,43 @@
+:- use_module(library(clpfd)).
+
+valid([]).
+
+valid([Head|Tail]) :-
+all_different(Head), % in the book, this is 'fd_all_different'
+valid(Tail).
+
+sudoku(Puzzle, Solution) :-
+Solution = Puzzle,
+Puzzle = [S11, S12, S13, S14,
+S21, S22, S23, S24,
+S31, S32, S33, S34,
+S41, S42, S43, S44],
+Puzzle ins 1..4, % in the book, this is 'fd_domain'
+Row1 = [S11, S12, S13, S14],
+Row2 = [S21, S22, S23, S24],
+Row3 = [S31, S32, S33, S34],
+Row4 = [S41, S42, S43, S44],
+Col1 = [S11, S21, S31, S41],
+Col2 = [S12, S22, S32, S42],
+Col3 = [S13, S23, S33, S43],
+Col4 = [S14, S24, S34, S44],
+Square1 = [S11, S12, S21, S22],
+Square2 = [S13, S14, S23, S24],
+Square3 = [S31, S32, S41, S42],
+Square4 = [S33, S34, S43, S44],
+valid([Row1, Row2, Row3, Row4,
+Col1, Col2, Col3, Col4,
+Square1, Square2, Square3, Square4]),
+pretty_print([Row1, Row2, Row3, Row4]).
+
+pretty_print([Head | Tail]) :- print(Head), 
+  print('\n'), pretty_print(Tail).
+
+problem(1,[_, _, 2, 3,
+_, _, _, _,
+_, _, _, _,
+3, 4, _, _]
+).
+
+% can test with:
+% ?- problem(1, Board), sudoku(Board, Solution).
